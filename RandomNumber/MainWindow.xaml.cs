60d31bd9a3ba7b46b0ea9c03e3b62b13a3a4186e
Orange-Icepop <czq13482103069@hotmail.com>
2024-08-30 17:18:13 +0800

    更新：避免重复抽取

--- RandomNumber/MainWindow.xaml.cs
+++ RandomNumber/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -59,7 +61,7 @@ namespace RandomNumber
             foreach (var item in skipNums)
             {
                 item.Trim();
-                if(int.TryParse(item, out int num) && num >= 0 && num <= 1000000)
+                if (int.TryParse(item, out int num) && num >= 0 && num <= 1000000)
                 {
                     SkipNumList.Add(num);
                 }
@@ -72,31 +74,46 @@ namespace RandomNumber
 
         private void RandNum(object sender, RoutedEventArgs e)
         {
-            if(MinNum.Text != "" && MaxNum.Text != "" && TimesNum.Text != "")
+            /*
+            if (MinNum.Text != "" && MaxNum.Text != "" && TimesNum.Text != "")
             {
-                if(int.TryParse(MinNum.Text, out int smallNum) && int.TryParse(MaxNum.Text, out int largeNum) && int.TryParse(TimesNum.Text, out int times))
+                if (int.TryParse(MinNum.Text, out int smallNum) && int.TryParse(MaxNum.Text, out int largeNum) && int.TryParse(TimesNum.Text, out int times))
                 {
-                    if(smallNum <= 1000000 && smallNum>= 0 && largeNum <= 1000000 && largeNum >= 0 && times <= 100 && times >= 0)
+                    if (smallNum <= 1000000 && smallNum >= 0 && largeNum <= 1000000 && largeNum >= 0 && times <= 100 && times >= 0)
                     {
                         if (smallNum <= largeNum)
                         {
-                            // 外边一圈全是校验，一环套一环，真的烦
-                            Random random = new Random();
-                            StringBuilder result = new StringBuilder();
-                            for (int i = 0; i < times; i++, result.Append("\n"))
-                            {
-                                int middle = random.Next(smallNum, largeNum + 1);
-                                if (!SkipNumList.Contains(middle))
-                                {
-                                    result.Append(middle);
-                                }
-                                else
-                                {
-                                    i--;
-                                }
-                            }
-                            string finalResult = result.ToString();
-                            MessageBox.Show(finalResult, "随机数结果", MessageBoxButton.OK, MessageBoxImage.Information);
+            */
+            if (Verify())
+            {
+                Random random = new Random();
+                List<string> resultList = new List<string>();
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < times; i++)
+                {
+                    int middle = random.Next(smallNum, largeNum + 1);
+                    if (SkipNumList.Contains(middle))
+                    {
+                        i--;
+                    }
+                    else if (AvoidRepeat.IsChecked == true && resultList.Contains(middle.ToString()))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        resultList.Add(middle.ToString());
+                        resultList.Add("\n");
+                    }
+                }
+                foreach (string item in resultList)
+                {
+                    result.Append(item);
+                }
+                string finalResult = result.ToString();
+                MessageBox.Show(finalResult, "随机数结果", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            /*
                         }
                         else
                         {
@@ -117,6 +134,55 @@ namespace RandomNumber
             {
                 MessageBox.Show("请输入数字范围", "错误：没有数值输入", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            */
         }
+
+        private bool Verify()
+        {
+            if (MinNum.Text == "" || MaxNum.Text == "" || TimesNum.Text == "")
+            {
+                MessageBox.Show("请输入数字范围", "错误：没有数值输入", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!int.TryParse(MinNum.Text, out smallNum) || !int.TryParse(MaxNum.Text, out largeNum) || !int.TryParse(TimesNum.Text, out times))
+            {
+                MessageBox.Show("请确保输入的是整数", "错误：数值输入格式错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (smallNum >= 1000000 || smallNum <= 0 || largeNum >= 1000000 || largeNum <= 0 || times >= 100 || times <= 0)
+            {
+                MessageBox.Show("请确保输入的数字在0~1,000,000以内且抽取数量在0~100以内", "错误：数值输入超过范围", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (smallNum >= largeNum)
+            {
+                MessageBox.Show("最大数值小于最小数值", "错误：数值大小错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (AvoidRepeat.IsChecked == true && largeNum - smallNum < times)
+            {
+                MessageBox.Show("避免重复抽取时，最大数值与最小数值之差应大于抽取数量", "错误：数值大小错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private int smallNum;
+        private int largeNum;
+        private int times;
+
+        /*
+        private void SaveConf(object sender, RoutedEventArgs e)
+        {
+            var conf = new
+            {
+                MinNum = MinNum.Text,
+                MaxNum = MaxNum.Text,
+                EnableSkipNum = EnableSkip.IsChecked,
+                SkipNum = SkipNum.Text,
+            }
+            File.WriteAllText("conf.json", "");
+        }
+        */
     }
 }
